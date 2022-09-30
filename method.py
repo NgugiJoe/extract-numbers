5cb42d9169b0f1a30fce75d56fe23f2e0b00873d
Joe <ngugijoenyaga@gmail.com>
2022-09-30 11:04:04 +0300

    added a txt file

---
+++ method.py
@@ -0,0 +1,28 @@
+import re
+
+handle=open('regex_sum_1624587.txt')
+numlist=list()
+
+for line in handle:
+    line=line.rstrip()    
+   # print(line)
+    invalue=re.findall('[0-9]+', line)
+   # print(invalue)
+    
+    
+    if len(invalue) <1 :
+        continue
+        
+    #print(invalue)
+    for value in invalue:
+       # print(value)
+        value=int(value)
+        #print(type(value))
+        
+        numlist.append(value)
+#print(numlist)
+
+sum=0
+for num in numlist:
+    sum=sum+num
+print(sum)
