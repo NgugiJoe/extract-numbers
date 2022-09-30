d4126a9181343b3f73fcb8d843c23df7c652741a
Joe <ngugijoenyaga@gmail.com>
2022-09-30 12:59:27 +0300

    working solution

---
+++ method.py
@@ -0,0 +1,31 @@
+import re
+
+handle=open('regex_sum_1624587.txt')
+numlist=list()
+
+for line in handle:
+    line=line.rstrip()    
+   # print(line)
+    invalue=re.findall('[0-9]+', line)
+   # print(invalue)
+    
+    
+    if len(invalue) <1 :
+        continue
+        
+    #print(invalue)
+    for value in invalue:
+       # print(value)
+        value=int(value)
+        #print(type(value))
+        
+        numlist.append(value)
+#print(numlist)
+
+sum=0
+for num in numlist:
+    sum=sum+num
+print(sum)
+
+#this is a second commit 
+print('this is a second commit point')
